dd645bb8f8554b5fc88125f481982c31bb1d6b6e
david hays <purplehays527@gmail.com>
2020-09-04 08:36:09 -0600

    seems to be working for this first card. time to get the loop working

--- git_cards/src/Card.js
+++ git_cards/src/Card.js
@@ -17,9 +17,11 @@ class Card extends React.Component{
             <div>
                 <p>UserName: {this.props.user.login}</p>
                 <img src={this.props.user.avatar_url}/>
-                <p>UserName: {this.props.user.login}</p>
-                <p>UserName: {this.props.user.login}</p>
-                <p>UserName: {this.props.user.login}</p>
+                <p>Bio: {this.props.user.bio}</p>
+                <p>Blog: {this.props.user.blog}</p>
+                <p>Followers: {this.props.user.followers}</p>
+                <p>Following: {this.props.user.following}</p>
+                <p>Repos: {this.props.user.public_repos}</p>
             </div>
         )
     }
